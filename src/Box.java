002ee2f3901ba66e8a76154d1793ebf8f410e8f1
SorokinAlexey1987 <alexey-sor@yandex.ru>
2022-01-09 00:50:39 +0300

    Object in parametr

--- src/Box.java
+++ src/Box.java
@@ -3,6 +3,24 @@ public class Box {
     double height;
     double length;
 
+    public Box(double width, double height, double length) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+    Box(double size) {
+        width = size;
+        height = size;
+        length = size;
+    }
+
+    Box(Box box) {
+        this.width = box.width;
+        this.height = box.height;
+        this.length = box.length;
+    }
+
     void showVolume() {
         double volume = width * height * length;
         System.out.println(volume);
@@ -10,5 +28,22 @@ public class Box {
 
     double volume() {
         return width * height * length;
+    }
+
+    int compare(Box box) {
+        double thisVolume = volume();
+        double boxVolume = box.volume();
+        int result;
+        if (thisVolume > boxVolume) {
+            //System.out.println("1>2");
+            result = 1;
+        } else if (thisVolume < boxVolume) {
+            //System.out.println("1<2");
+            result = -1;
+        } else {
+            //System.out.println("1=2");
+            result = 0;
+        }
+        return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Box box1 = new Box();
-        Box box2 = new Box();
-        box1.width = 5;
-        box1.length = 5;
-        box1.height = 5;
-        box2.width = 10;
-        box2.length = 10;
-        box2.height = 10;
+        //Box box1 = new Box(10, 10, 10);
+        //Box box2 = new Box(15, 20, 25);
+        Box box1 = new Box(10);
+        Box box2 = new Box(box1);
         //box1.showVolume();
         //box2.showVolume();
         double volume1 = box1.volume();
@@ -15,6 +11,7 @@ public class Main {
         //System.out.println("Объем 1 = " + volume1);
         //System.out.println("Объем 2 = " + volume2);
 
+        /*
         Dog dog1 = new Dog();
         Dog dog2 = new Dog();
         dog1.name = "Барбос";
@@ -27,5 +24,19 @@ public class Main {
         dog2.run();
         System.out.println(dog1.info());
         System.out.println(dog2.info());
+        */
+
+        int result = box1.compare(box2);
+        switch (result) {
+            case -1:
+                System.out.println("1<2");
+                break;
+            case 0:
+                System.out.println("1=2");
+                break;
+            case 1:
+                System.out.println("1>2");
+                break;
+        }
     }
 }
